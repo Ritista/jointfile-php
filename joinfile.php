23c17fedf9e3ccb93f3ffcb07138b8a535fd00ff
Ritista Shrestha <ritista99@gmail.com>
2024-07-13 22:10:26 +0545

    first commit

---
+++ joinfile.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name ="viewport" content ="width=device-width, initial-scale=1.0">
+<title>document</title>  
+</head>
+<body>
+<h2>php form validation example</h2>
+<form method ="POST" action="welcome.php">
+name:<input type="text" name="name">
+<br><br>
+email:<input type="text" name="email">
+<br><br>
+website:<input type="text" name="website">
+<br><br>
+comment:<textarea name ="comment" rows="s" cols="40"></textarea>
+<br><br>
+gender:<input type="radio" name="gender" value="female">female
+<input type="radio" name="gender" value="male">male
+<input type="radio" name="gender" value="other">other
+<br><br>
+<input type ="submit" name="submit" value="submit">
+</form>
+</body>
+</html>
